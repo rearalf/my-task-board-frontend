68b98f2e97bfe6ffc5bed7819f6c9014161dbe87
Ricardo Alf <rear_recinos@hotmail.com>
2024-12-18 09:07:55 -0600

    feat: Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+
+const vuetify = createVuetify()
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 
 app.mount('#app')
